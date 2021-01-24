864e1539a7e4cf58faebccfb26f6a33ffeb027f1
gventurini <intesasanpaolo>
2021-01-24 16:12:57 +0100

    add default output template

--- server/src/main.py
+++ server/src/main.py
@@ -1,6 +1,11 @@
 import youtube_dl
+from pathlib import Path
 
+default_download_folder = Path.home().joinpath('Downloads', 'youtube-dl')
+default_output_template = '%(title)s.%(ext)s'
 ydl_opts = {
+    'outtmpl': str(default_download_folder.joinpath(default_output_template))
 }
+
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
