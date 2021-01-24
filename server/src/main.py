f13a35c3a11a5cc082a0b9a267bd4876e23cbb77
gventurini <intesasanpaolo>
2021-01-24 17:06:46 +0100

    add base flask api that download a sample youtube video

--- server/src/main.py
+++ server/src/main.py
@@ -1,5 +1,6 @@
 import youtube_dl
 from pathlib import Path
+from flask import Flask
 
 default_download_folder = Path.home().joinpath('Downloads', 'youtube-dl')
 default_output_template = '%(title)s.%(ext)s'
@@ -7,5 +8,18 @@ ydl_opts = {
     'outtmpl': str(default_download_folder.joinpath(default_output_template))
 }
 
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
+app = Flask(__name__)
+
+base_route = '/youtube-dl'
+
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route(f'${base_route}/download', method=['POST'])
+def download():
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
+    return 'success'
