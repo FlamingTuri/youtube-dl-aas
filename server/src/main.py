42b21bba5318d8db000c2ac18d235969d9c2e4cc
gventurini <intesasanpaolo>
2021-01-24 17:56:20 +0100

    add api to download a video with the specified options

--- server/src/main.py
+++ server/src/main.py
@@ -1,10 +1,13 @@
 import youtube_dl
 from pathlib import Path
 from flask import Flask
+from flask import request
+from flask import abort
 
+bad_request_code = 400
 default_download_folder = Path.home().joinpath('Downloads', 'youtube-dl')
 default_output_template = '%(title)s.%(ext)s'
-ydl_opts = {
+default_ydl_opts = {
     'outtmpl': str(default_download_folder.joinpath(default_output_template))
 }
 
@@ -18,8 +21,17 @@ def index():
     return 'Index Page'
 
 
-@app.route(f'${base_route}/download', method=['POST'])
+@app.route(f'{base_route}/download', methods=['POST'])
 def download():
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
-    return 'success'
+    if request.is_json:
+        body = request.get_json()
+        url = body.get('url')
+        if url is None:
+            abort(bad_request_code, '\'url\' key not defined')
+
+        ydl_opts = body.get('options', default_ydl_opts)
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        return 'success'
+    else:
+        abort(bad_request_code, 'body must be in json format')
