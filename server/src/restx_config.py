cc0609a9e6037ee8d1c607ece9126b0226edb98d
gventurini <gventurini@reply.it>
2021-03-03 23:40:55 +0100

    add model validation

--- server/src/restx_config.py
+++ server/src/restx_config.py
@@ -5,6 +5,8 @@ from flask_restx import Api
 log = logging.getLogger(__name__)
 
 app = Flask(__name__)
+app.config['RESTX_VALIDATE'] = True
+
 api = Api(version='1.0', title='Youtube dl as a service API')
 api.init_app(app)
 
