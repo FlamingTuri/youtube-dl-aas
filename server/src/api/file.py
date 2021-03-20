af7fa73a4f880df29187e23f3e6b634dcb39a036
gventurini <gventurini@reply.it>
2021-03-20 10:37:44 +0100

    remove code used to zip files

--- server/src/api/file.py
+++ server/src/api/file.py
@@ -1,22 +1,18 @@
-import youtube_dl
-from pathlib import Path
-from src.youtube_dl_progress_hook import YoutubeDlProgressHook
+from src.service.downloader_service import DownloaderService
 from src.restx_config import api
 from flask_restx import Resource, fields
-from src.config import Config
-from flask import send_from_directory, safe_join, send_file
-import zipfile
-import os
+from flask import send_from_directory, send_file
 
 ns = api.namespace('youtube-dl', description='Download operations')
 
-download_folder = Config().get_download_folder()
+downloader_service = DownloaderService()
 
 @ns.route('/file/<name>')
 class File(Resource):
 
     def get(self, name):
         try:
+            download_folder = downloader_service.config.get_download_folder()
             return send_from_directory(download_folder, filename=name, as_attachment=True)
         except FileNotFoundError:
             return abort(404)
@@ -34,24 +30,12 @@ class Files(Resource):
         requested_files = set(body)
 
         zipfile_name = 'ZipFile.zip'
-        with zipfile.ZipFile(zipfile_name,'w', zipfile.ZIP_DEFLATED) as zipf:
-            for file in requested_files:
-                file_path = safe_join(download_folder, file)
-                zipf.write(file_path)
-            zipf.close()
-        
-        with open(zipfile_name, 'rb') as f:
-            data = f.readlines()
 
-        result = send_file(
-            data,
+        zip_file = downloader_service.zip_files(requested_files)
+
+        return send_file(
+            zip_file,
             mimetype='application/zip',
             as_attachment=True,
             attachment_filename=zipfile_name
         )
-        os.remove(zipfile_name)
-        return result
-        #return Response(data, headers={
-        #    'Content-Type': 'application/zip',
-        #    'Content-Disposition': 'attachment; filename=%s;' % zipfile_name
-        #})     
