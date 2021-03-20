c4b6f7ff83d6c7fc7fe4718ec1efc6dc0d288c88
gventurini <gventurini@reply.it>
2021-03-20 12:05:15 +0100

    add multiple files download method and file name retrieval from response headers

--- web-gui/src/app/app.component.ts
+++ web-gui/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { saveAs } from 'file-saver';
+import { DownloadService } from './download.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +14,24 @@ export class AppComponent {
     'url2'
   ];
 
+  constructor(private downloadService: DownloadService) { }
+
   download() {
-    
+    let response: Promise<HttpResponse<Blob>>;
+    if (this.urls.length === 1) {
+      const url = this.urls[0];
+      response = this.downloadService.getFile(url);
+    } else {
+      response = this.downloadService.getFiles(this.urls)
+    }
+    response.then(response => this.promptSaveData(response));
+  }
+
+  promptSaveData(response: HttpResponse<Blob>) {
+    const fileName = this.downloadService.getFileNameFromHeaders(response.headers)
+    const data = response.body;
+    if (data !== null) {
+      saveAs(data, fileName)
+    }
   }
 }
--- web-gui/src/app/download.service.ts
+++ web-gui/src/app/download.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,27 @@ export class DownloadService {
 
   constructor(private httpClient: HttpClient) { }
 
-  download(name: string) {
-    return this.httpClient.get(this.base + '/file' + `/${name}`, {responseType: 'blob'}).toPromise();
+  getFile(name: string): Promise<HttpResponse<Blob>> {
+    return this.httpClient.get(this.base + '/file' + `/${name}`,
+      { responseType: 'blob', observe: 'response' }).toPromise();
+  }
+
+  getFiles(names: string[]): Promise<HttpResponse<Blob>> {
+    if (names.length === 1) {
+      return this.getFile(names[0]);
+    } else {
+      return this.httpClient.post(this.base + '/files', names,
+        { responseType: 'blob', observe: 'response' }).toPromise();
+    }
+  }
+
+  getFileNameFromHeaders(headers: HttpHeaders): string {
+    const contenDisposition = headers.get('content-disposition');
+    if (contenDisposition === null) {
+      return 'unknown';
+    }
+    // https://stackoverflow.com/a/23054920/7380828
+    const fileName = contenDisposition.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/);
+    return fileName === null ? 'unknown' : fileName[1];
   }
 }
