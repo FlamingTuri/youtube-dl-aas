6d56012481b0db8b98243bf44a019febe606953f
gventurini <gventurini@reply.it>
2021-03-20 10:32:54 +0100

    add method to zip files in memory

--- server/src/service/downloader_service.py
+++ server/src/service/downloader_service.py
@@ -1,6 +1,9 @@
 import youtube_dl
 from src.youtube_dl_progress_hook import YoutubeDlProgressHook
 from src.config import Config
+from io import BytesIO
+import zipfile
+from flask import safe_join
 
 class DownloaderService:
 
@@ -23,3 +26,15 @@ class DownloaderService:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download(urls)
             return self.progress_hook.get_downloaded_files_locations()
+
+    def zip_files(self, files: list):
+        download_folder = self.config.get_download_folder()
+        
+        in_memory_zip = BytesIO()
+        with zipfile.ZipFile(in_memory_zip, 'w', zipfile.ZIP_DEFLATED) as zf:
+            for file in files:
+                data = zipfile.ZipInfo(safe_join(download_folder, file))
+                zf.writestr(data, file)
+        in_memory_zip.seek(0)
+
+        return in_memory_zip
