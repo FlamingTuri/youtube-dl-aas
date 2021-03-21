4b890c59081f6798ff92458d1bad66db28a24b9d
gventurini <gventurini@reply.it>
2021-03-21 20:12:01 +0100

    load methods now return the loaded file name

--- server/src/service/downloader_service.py
+++ server/src/service/downloader_service.py
@@ -5,6 +5,7 @@ from io import BytesIO
 import zipfile
 from flask import safe_join
 import os
+from src.models.file import File
 
 class DownloaderService:
 
@@ -13,7 +14,7 @@ class DownloaderService:
     config = Config()
     default_output_template = '%(title)s.%(ext)s'
 
-    def download(self, urls: set, ydl_opts: dict = None, temporary: bool = False) -> list: 
+    def download(self, urls: set, ydl_opts: dict = None, temporary: bool = False) -> str: 
         download_folder = self.config.get_download_folder(temporary)
         outtmpl = self.config.get_as_path_in_download_folder(default_output_template, download_folder)
         default_ydl_opts = { 'outtmpl': outtmpl }
@@ -29,7 +30,7 @@ class DownloaderService:
             # it is not possible and the download location is returned instead
             return download_folder
 
-    def zip_files(self, files: list, download_folder: str = config.get_download_folder()) -> BytesIO:
+    def zip_files(self, files: list, download_folder: str = config.get_download_folder()) -> File:
         in_memory_zip = BytesIO()
         with zipfile.ZipFile(in_memory_zip, 'w', zipfile.ZIP_DEFLATED) as zf:
             for file in files:
@@ -37,16 +38,17 @@ class DownloaderService:
                 zf.writestr(data, file)
         in_memory_zip.seek(0)
 
-        return in_memory_zip
+        now = self.config.get_current_time_as_string()
+        return File(f'ytdl-download-{now}.zip', in_memory_zip)
 
-    def load_file(self, file: str, download_folder: str = config.get_download_folder()) -> BytesIO:
+    def load_file(self, file: str, download_folder: str = config.get_download_folder()) -> File:
         with open(safe_join(download_folder, file), 'rb') as fh:
-            return BytesIO(fh.read())
+            return File(file, BytesIO(fh.read()))
 
     def remove_file(self, file: str, download_folder: str = config.get_download_folder()) -> None:
         os.remove(safe_join(download_folder, name))
 
-    def load_files_in_directory(self, directory: str = config.get_download_folder()) -> BytesIO:
+    def load_files_in_directory(self, directory: str = config.get_download_folder()) -> File:
         files = os.listdir(directory)
         files_number = len(files)
         if files_number == 1:
@@ -54,4 +56,4 @@ class DownloaderService:
         elif files_number > 1:
             return zip_files(files, directory)
         else:
-            raise Exception(f'no files found in directory ${directory}')
+            raise Exception(f'no files found in directory {directory}')
