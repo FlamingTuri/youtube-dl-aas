013187aae08aa390189d15aff7d6688e8aed575d
gventurini <gventurini@reply.it>
2021-03-21 19:20:04 +0100

    change download method to permit the creation of a temporary folder

--- server/src/service/downloader_service.py
+++ server/src/service/downloader_service.py
@@ -13,20 +13,21 @@ class DownloaderService:
     config = Config()
     default_output_template = '%(title)s.%(ext)s'
 
-    default_ydl_opts = {
-        'progress_hooks': [progress_hook.progress_hook],
-        'outtmpl': config.get_as_path_in_download_folder(default_output_template)
-    }
-
-    def download(self, urls: set, ydl_opts: dict = None) -> list: 
+    def download(self, urls: set, ydl_opts: dict = None, temporary: bool = False) -> list: 
+        download_folder = self.config.get_download_folder(temporary)
+        outtmpl = self.config.get_as_path_in_download_folder(default_output_template, download_folder)
+        default_ydl_opts = { 'outtmpl': outtmpl }
         if ydl_opts is None or not ydl_opts:
-            ydl_opts = self.default_ydl_opts
+            ydl_opts = default_ydl_opts
         else:
-            ydl_opts.update(self.default_ydl_opts)
+            ydl_opts.update(default_ydl_opts)
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download(urls)
-            return self.progress_hook.get_downloaded_files_locations()
+            # ideally the downloaded files should be returned, however, 
+            # due to https://github.com/ytdl-org/youtube-dl/issues/5710,
+            # it is not possible and the download location is returned instead
+            return download_folder
 
     def zip_files(self, files: list) -> BytesIO:
         download_folder = self.config.get_download_folder()
