3c9c64a39ec7cd3ce423fc60ed2a3a6fe9d0e64f
gventurini <gventurini@reply.it>
2021-03-21 19:18:39 +0100

    add possibility to generate a temporary download folder path

--- server/src/config.py
+++ server/src/config.py
@@ -1,12 +1,18 @@
 from pathlib import Path
+from datetime import datetime
 
 class Config:
 
     __default_download_folder = Path.home().joinpath('Downloads', 'youtube-dl')
 
-    def get_download_folder(self) -> str:
-        return str(self.__default_download_folder)
+    __time_format = '%Y-%m-%d h%H m%M s%S'
 
-    def get_as_path_in_download_folder(self, path: str) -> str:
+    def get_download_folder(self, temporary: bool = False) -> str:
+        if temporary:
+            now = datetime.now().strftime(self.__time_format)
+            return str(self.__default_download_folder.joinpath(now))
+        else:
+            return str(self.__default_download_folder)
+
+    def get_as_path_in_download_folder(self, path: str, download_folder: str = __default_download_folder) -> str:
         return str(self.__default_download_folder.joinpath(path))
-    
