8e8ec5b5f9e0c7e92e2bfa0ac79b3bf10d572d56
gventurini <giacomo.venturini3@gmail.com>
2021-08-26 23:20:19 +0200

    fix json returned by doc api

--- server/src/api/doc.py
+++ server/src/api/doc.py
@@ -5,10 +5,18 @@ from src.service.doc_service import DocService
 
 ns = api.namespace('youtube-dl', description='Youtube dl documentation')
 
+doc = api.model(
+    "Doc",
+    {
+        "content": fields.String(required=True, description="Documentation returned by Youtube dl library"),
+    },
+)
+
 @ns.route('/doc')
 class Doc(Resource):
 
     doc_service = DocService()
 
+    @ns.marshal_with(doc)
     def get(self):
-        return self.doc_service.get_youtube_dl_doc().__dict__
+        return self.doc_service.get_youtube_dl_doc()
