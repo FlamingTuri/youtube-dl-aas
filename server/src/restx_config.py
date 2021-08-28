565c80bf53407f9f6d1e28d6ee90dd7ddb0421d9
gventurini <giacomo.venturini3@gmail.com>
2021-08-28 22:05:14 +0200

    add serve of static files

--- server/src/restx_config.py
+++ server/src/restx_config.py
@@ -1,18 +1,22 @@
 import logging
-from flask import Flask
+from flask import Flask, render_template
 from flask_restx import Api
 from flask_cors import CORS
 
 log = logging.getLogger(__name__)
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='resources/static', template_folder='resources/templates')
 app.config['RESTX_VALIDATE'] = True
 app.config['CORS_EXPOSE_HEADERS'] = 'Content-Disposition'
 CORS(app)
 
-api = Api(version='1.0', title='Youtube dl as a service API')
+api = Api(version='1.0', title='Youtube dl as a service API', doc='/swagger')
 api.init_app(app)
 
+
+@app.route('/home', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 @api.errorhandler
 def default_error_handler(error: Exception):
