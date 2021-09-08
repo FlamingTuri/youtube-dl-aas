942c9f5861e5d805d5f0abccdca78216579d7af6
gventurini <giacomo.venturini3@gmail.com>
2021-09-08 21:39:18 +0200

    add api to get host address

--- server/src/config/flask_config.py
+++ server/src/config/flask_config.py
@@ -29,15 +29,17 @@ def init_api():
 
     api.init_app(app)
 
+    from src.api.doc import ns as doc_namespace
     from src.api.download import ns as download_namespace
     from src.api.download_and_send import ns as download_and_send_namespace
     from src.api.file import ns as file_namespace
-    from src.api.doc import ns as doc_namespace
+    from src.api.info import ns as info_namespace
     from src.api.version import ns as version_namespace
+    api.add_namespace(doc_namespace)
     api.add_namespace(download_namespace)
     api.add_namespace(download_and_send_namespace)
     api.add_namespace(file_namespace)
-    api.add_namespace(doc_namespace)
+    api.add_namespace(info_namespace)
     api.add_namespace(version_namespace)
 
 
