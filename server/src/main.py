247cda9c0f87227d798daf3c20de63c76518fb79
gventurini <giacomo.venturini3@gmail.com>
2021-09-08 21:38:42 +0200

    add print of swagger link

--- server/src/main.py
+++ server/src/main.py
@@ -2,6 +2,7 @@ from src.config.flask_config import app
 
 
 def start_flask_app():
+    print(" * App swagger will be available at: http://127.0.0.1:5000/swagger")
     app.run(host="0.0.0.0", debug=False)
 
 
