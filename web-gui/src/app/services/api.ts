a218e6a9a2e47abea5189e9ca91a7c3646233c94
gventurini <giacomo.venturini3@gmail.com>
2021-09-23 22:51:54 +0200

    fix #25 - the server hostname and port are now taken from the page url

--- web-gui/src/app/services/api.ts
+++ web-gui/src/app/services/api.ts
@@ -2,5 +2,9 @@ import { UrlBuilder } from 'http-url-builder';
 
 export class Api {
   
-  static readonly base = new UrlBuilder('http://0.0.0.0:5000/youtube-dl');
+  static readonly protocol = window.location.protocol;
+  static readonly hostname = window.location.hostname;
+  static readonly port = window.location.port;
+  static readonly baseUrl =`${Api.protocol}//${Api.hostname}:${Api.port}/youtube-dl`
+  static readonly base = new UrlBuilder(Api.baseUrl);
 }
