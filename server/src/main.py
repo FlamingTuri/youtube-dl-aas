20c6d348143c418a635e946c8ddf25cfcb767a5b
gventurini <giacomo.venturini3@gmail.com>
2022-05-30 22:54:46 +0200

    add port configuration from input argv

--- server/src/main.py
+++ server/src/main.py
@@ -1,10 +1,13 @@
+import sys
 from src.config.flask_config import app, init_api
 
 
 def start_flask_app(fs_storage: bool = True):
-    print(" * App swagger will be available at: http://127.0.0.1:5000/swagger")
+    print(f"Input argv: ${str(sys.argv)}")
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    print(f" * App swagger will be available at: http://127.0.0.1:${port}/swagger")
     init_api(fs_storage)
-    app.run(host="0.0.0.0", debug=False)
+    app.run(host="0.0.0.0", debug=False, port=port)
 
 
 if __name__ == '__main__':
