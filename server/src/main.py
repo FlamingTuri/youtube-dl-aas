2876af6420470469ed5cc8a3f5be98b8850e6141
gventurini <giacomo.venturini3@gmail.com>
2022-06-07 23:34:19 +0200

    add logging of input argv

--- server/src/main.py
+++ server/src/main.py
@@ -4,6 +4,7 @@ from src.config.flask_config import app, init_api
 
 def start_flask_app(fs_storage: bool = True):
     print(f"Input argv: ${str(sys.argv)}")
+    print(f"Input argv: ${str(sys.argv)}")
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
     print(f" * App swagger will be available at: http://127.0.0.1:${port}/swagger")
     init_api(fs_storage)
