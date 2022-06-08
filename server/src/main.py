042b0f6170142207ac5e2f3e6a8214adc40276d8
gventurini <giacomo.venturini3@gmail.com>
2022-06-08 23:41:39 +0200

    fix lint errors

--- server/src/main.py
+++ server/src/main.py
@@ -1,14 +1,16 @@
 import sys
 from src.config.flask_config import app, init_api
 
+
 def get_argv_param(key: str, default_value: str = None) -> str:
     value = next(filter(lambda arg: key in arg, sys.argv), default_value)
-    return value if value == None else value.removeprefix(key)
+    return value if value is None else value.removeprefix(key)
+
 
 def start_flask_app(fs_storage: bool = True):
     print(f"Input argv: ${str(sys.argv)}")
-    port=int(get_argv_param('--port=', '5000'))
-    ssl_context=get_argv_param('--ssl_context=', None)
+    port = int(get_argv_param('--port=', '5000'))
+    ssl_context = get_argv_param('--ssl_context=', None)
     print(f" * App swagger will be available at: http://127.0.0.1:${port}/swagger")
     init_api(fs_storage)
     app.run(host="0.0.0.0", debug=False, port=port, ssl_context=ssl_context)
